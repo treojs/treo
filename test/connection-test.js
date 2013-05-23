682bb6f6a45454fa4649c24ebcd025f29b6a0940
Aleksey Kulikov <AlekseyKulikov@hotmail.com>
2013-05-23 16:28:16 +0600

    Updated tests for new api

--- test/connection-test.js
+++ test/connection-test.js
@@ -1,20 +1,20 @@
 describe('Connection and schema management', function() {
-  var expect     = chai.expect;
-  var localStore = require('store');
-  var Indexed    = require('indexed');
+  var expect  = chai.expect;
+  var store   = require('store');
+  var Indexed = require('indexed');
 
   it('connect to multiply stores in one db', function(done) {
     Indexed.drop('testapp1', function(err) {
-      var notes    = Indexed.create('testapp1:notes');
-      var tags     = Indexed.create('testapp1:tags');
-      var notepads = Indexed.create('testapp1:notepads');
+      var notes    = new Indexed('testapp1:notes');
+      var tags     = new Indexed('testapp1:tags');
+      var notepads = new Indexed('testapp1:notepads');
 
       async.series([
-        function(cb) { notes(cb); },
-        function(cb) { tags(cb); },
-        function(cb) { notepads(cb); }
+        function(cb) { notes.all(cb); },
+        function(cb) { tags.all(cb); },
+        function(cb) { notepads.all(cb); }
       ], function(err) {
-        var config = localStore('indexed-testapp1');
+        var config = store('indexed-testapp1');
         expect(config.stores).length(3);
         expect(Object.keys(config.keys)).eql(['notes', 'tags', 'notepads']);
         expect(config.version).equal(4);
@@ -25,15 +25,15 @@ describe('Connection and schema management', function() {
 
   it('updates db.version when key was changed', function(done) {
     Indexed.drop('testapp2', function(err1) {
-      var notes = Indexed.create('testapp2:notes', { key: '_id' });
-      notes(1, { name: 'note 1' }, function(err2) {
-        var changedNotes = Indexed.create('testapp2:notes', { key: 'id' });
+      var notes = new Indexed('testapp2:notes', { key: '_id' });
+      notes.put(1, { name: 'note 1' }, function(err2) {
+        var changedNotes = new Indexed('testapp2:notes', { key: 'id' });
 
         async.series([
-          function(cb) { changedNotes(cb); },
-          function(cb) { notes(cb); }
+          function(cb) { changedNotes.all(cb); },
+          function(cb) { notes.all(cb); }
         ], function(err3, result) {
-          var config = localStore('indexed-testapp2');
+          var config = store('indexed-testapp2');
 
           expect(result).length(2);
           expect(result[0]).length(0);
@@ -46,12 +46,14 @@ describe('Connection and schema management', function() {
 
   it('handles empty localStorage', function(done) {
     Indexed.drop('testapp3', function(err1) {
-      Indexed.create('testapp2:notes')(function(err, all) {
-        localStore('indexed-testapp3', null);
+      var notes = new Indexed('testapp2:notes');
+      notes.all(function(err2, all) {
+        store('indexed-testapp3', null);
 
-        Indexed.create('testapp3:notes')(function(err2, all) {
-          expect(localStore('indexed-testapp3').version).equal(2);
-          done(err1 || err2);
+        notes = new Indexed('testapp3:notes');
+        notes.all(function(err3, all) {
+          expect(store('indexed-testapp3').version).equal(2);
+          done(err1 || err2 || err3);
         });
       });
     });
@@ -63,18 +65,18 @@ describe('Connection and schema management', function() {
       function(cb) { Indexed.drop('testapp5', cb); },
       function(cb) { Indexed.drop('testapp6', cb); }
     ], function(err1) {
-      var notes    = Indexed.create('testapp4:notes');
-      var tags     = Indexed.create('testapp5:tags');
-      var notepads = Indexed.create('testapp6:notepads');
+      var notes    = new Indexed('testapp4:notes');
+      var tags     = new Indexed('testapp5:tags');
+      var notepads = new Indexed('testapp6:notepads');
 
       async.series([
-        function(cb) { notes(cb); },
-        function(cb) { tags(cb); },
-        function(cb) { notepads(cb); }
+        function(cb) { notes.all(cb); },
+        function(cb) { tags.all(cb); },
+        function(cb) { notepads.all(cb); }
       ], function(err2) {
-        expect(localStore('indexed-testapp4').version).equal(2);
-        expect(localStore('indexed-testapp5').version).equal(2);
-        expect(localStore('indexed-testapp6').version).equal(2);
+        expect(store('indexed-testapp4').version).equal(2);
+        expect(store('indexed-testapp5').version).equal(2);
+        expect(store('indexed-testapp6').version).equal(2);
         done(err1 || err2);
       });
     });
--- test/crud-test.js
+++ test/crud-test.js
@@ -1,15 +1,15 @@
-describe('Indexed.create instance', function(){
+describe('new Indexed instance', function(){
   var expect  = chai.expect;
   var Indexed = require('indexed');
-  var indexed = Indexed.create('notepad:notes', { key: '_id' });
+  var indexed = new Indexed('notepad:notes', { key: '_id' });
 
   beforeEach(function(done) {
-    indexed(null, done);
+    indexed.clear(done);
   });
 
-  describe('indexed(cb)', function() {
+  describe('all', function() {
     it('returns an empty array', function(done) {
-      indexed(function(err, values) {
+      indexed.all(function(err, values) {
         expect(values).length(0);
         done(err);
       });
@@ -17,12 +17,12 @@ describe('Indexed.create instance', function(){
 
     it('returns a list of objects', function(done) {
       async.series([
-        function(cb) { indexed(1, { name: 'note 1' }, cb); },
-        function(cb) { indexed(2, { name: 'note 2' }, cb); },
-        function(cb) { indexed(3, { name: 'note 3' }, cb); },
-        function(cb) { indexed(4, { name: 'note 4' }, cb); }
+        function(cb) { indexed.put(1, { name: 'note 1' }, cb); },
+        function(cb) { indexed.put(2, { name: 'note 2' }, cb); },
+        function(cb) { indexed.put(3, { name: 'note 3' }, cb); },
+        function(cb) { indexed.put(4, { name: 'note 4' }, cb); }
       ], function(err1) {
-        indexed(function(err2, notes) {
+        indexed.all(function(err2, notes) {
           expect(notes).length(4);
           done(err1 || err2);
         });
@@ -30,118 +30,58 @@ describe('Indexed.create instance', function(){
     });
   });
 
-  describe('indexed(key, cb)', function() {
-    it('gets value by key', function(done) {
-      indexed(1, { name: 'note 1' }, function(err1) {
-        indexed(1, function(err2, note) {
-          expect(note._id).equal(1);
-          expect(note.name).equal('note 1');
-          done(err1 || err2);
-        });
-      });
-    });
-
-    it('returns undefined when key is not found', function(done) {
-      indexed(5, function(err, note) {
-        expect(note).undefined;
-        done(err);
-      });
-    });
-
-    it('clears store when key is null', function(done) {
-      async.series([
-        function(cb) { indexed(1, { name: 'note 1' }, cb); },
-        function(cb) { indexed(2, { name: 'note 2' }, cb); },
-        function(cb) { indexed(3, { name: 'note 3' }, cb); },
-        function(cb) { indexed(null, cb); }
-      ], function(err1) {
-        indexed(function(err2, notes) {
-          expect(notes).length(0);
-          done(err1 || err2);
-        });
-      });
-    });
-  });
-
-  describe('indexed(key, val, cb)', function() {
-    it('puts value to store', function(done) {
-      indexed(5, { name: 'note 5' }, function(err, note) {
-        expect(note.name).equal('note 5');
-        expect(err).null;
-        done(err);
-      });
-    });
-
-    it('delete objetc by key if val is null', function(done) {
-      indexed(5, { name: 'note 5' }, function(err1) {
-        indexed(5, null, function(err2) {
-          indexed(function(err3, notes) {
-            expect(notes).length(0);
-            done(err1 || err2 || err3);
-          });
-        });
-      });
-    });
-  });
-
-  describe('shortcut methods', function() {
+  describe('get', function() {
     beforeEach(function(done) {
       async.series([
-        function(cb) { indexed(1, { name: 'note 1' }, cb); },
-        function(cb) { indexed(2, { name: 'note 2' }, cb); },
-        function(cb) { indexed(3, { name: 'note 3' }, cb); }
+        function(cb) { indexed.put(1, { name: 'note 1' }, cb); },
+        function(cb) { indexed.put(2, { name: 'note 2' }, cb); },
+        function(cb) { indexed.put(3, { name: 'note 3' }, cb); }
       ], done);
     });
 
-    it('all - returns all values', function(done) {
-      indexed.all(function(err, notes) {
-        expect(notes).length(3);
-        done(err);
-      });
-    });
-
-    it('get - returns one value', function(done) {
+    it('returns one value', function(done) {
       indexed.get(2, function(err, note) {
         expect(note._id).equal(2);
         done(err);
       });
     });
 
-    it('put - updates existing value', function(done) {
-      indexed.put(3, { name: 'updated note 3' }, function(err, note) {
-        expect(note._id).equal(3);
+    it('returns undefined when key is not found', function(done) {
+      indexed.get(5, function(err, note) {
+        expect(note).undefined;
+        done(err);
+      });
+    });
+  });
+
+  describe('put', function() {
+    it('updates value in the store', function(done) {
+      indexed.put(5, { name: 'note 5' }, function(err, note) {
+        expect(note.name).equal('note 5');
         expect(err).null;
         done(err);
       });
     });
+  });
 
-    it('del - delete object by key', function(done) {
-      indexed.del(2, function(err1) {
-        indexed.all(function(err2, notes) {
-          expect(notes).length(2);
-          done(err1 || err2);
-        });
-      });
-    });
-
-    it('clear - delete all objects', function(done) {
-      indexed.clear(function(err1) {
-        indexed.all(function(err2, notes) {
-          expect(notes).length(0);
-          done(err1 || err2);
-        });
-      });
+  describe('del', function() {
+    it('deletes object by key', function(done) {
+      async.series([
+        function(cb) { indexed.put(5, { name: 'note 5' }, cb); },
+        function(cb) { indexed.del(5, cb); },
+        function(cb) { indexed.all(function(err, notes) {expect(notes).length(0); cb(err); }); }
+      ], done);
     });
   });
 
   describe('keys', function() {
     beforeEach(function(done) {
       async.series([
-        function(cb) { indexed([1], { name: 'note 1' }, cb); },
-        function(cb) { indexed([2], { name: 'note 2' }, cb); },
-        function(cb) { indexed('key', { name: 'note 3' }, cb); },
-        function(cb) { indexed(['doom', 3, [1, 2]], { name: 'note 4' }, cb); },
-        function(cb) { indexed('a b c', { name: 'note 5' }, cb); }
+        function(cb) { indexed.put([1], { name: 'note 1' }, cb); },
+        function(cb) { indexed.put([2], { name: 'note 2' }, cb); },
+        function(cb) { indexed.put('key', { name: 'note 3' }, cb); },
+        function(cb) { indexed.put(['doom', 3, [1, 2]], { name: 'note 4' }, cb); },
+        function(cb) { indexed.put('a b c', { name: 'note 5' }, cb); }
       ], done);
     });
 
@@ -163,26 +103,22 @@ describe('Indexed.create instance', function(){
   });
 
   describe('errors', function() {
-    it('throws error when cb is not defined', function() {
-      expect(indexed).throw(/callback required/);
-    });
-
     it('validates name paramether', function() {
       expect(function() {
-        Indexed.create('smth here');
+        new Indexed('smth here');
       }).throw(/format/);
     });
 
     it('returns error when key is invalid', function(done) {
       indexed.put({ key: 1 }, {}, function(err, note) {
-        expect(err).not.null;
+        expect(err).exists;
         done();
       });
     });
 
     it('throws error when cb is not a function', function() {
       expect(function() {
-        indexed('smth here');
+        indexed.all('smth here');
       }).throw(/callback/);
     });
 
