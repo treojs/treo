53ae2afb2f3fe67ae3e4ccab79ef72cb2413bd77
Aleksey Kulikov <AlekseyKulikov@hotmail.com>
2014-07-04 18:05:50 +0400

    bunch of fixes

--- examples/find-in.js
+++ examples/find-in.js
@@ -1,3 +1,4 @@
+var treo = require('treo');
 
 /**
  * Efficient way to get bunch of records by `keys`.
@@ -23,19 +24,22 @@
 module.exports = function findIn(store, keys, cb) {
   var result = [];
   var current = 0;
-  keys = keys.sort();
+  keys = keys.sort(treo.cmp);
 
   store.cursor({ iterator: iterator }, done);
 
   function iterator(cursor) {
-    while (cursor.key > keys[current] && current <= keys.length) current++;
     if (current > keys.length) return done();
-
-    if (cursor.key === keys[current]) {
-      result.push(cursor.value);
-      cursor.continue();
-    } else {
+    if (cursor.key > keys[current]) {
+      result.push(undefined); // key not found
+      current += 1;
       cursor.continue(keys[current]);
+    } else if (cursor.key === keys[current]) {
+      result.push(cursor.value); // key found
+      current += 1;
+      cursor.continue(keys[current]);
+    } else {
+      cursor.continue(keys[current]); // go to next key
     }
   }
 
