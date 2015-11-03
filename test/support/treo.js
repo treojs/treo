52cf44131aa5f78e76aebfbaec9259266fef50b1
Aleksey Kulikov <alekseys.kulikov@gmail.com>
2015-11-03 17:10:35 +0100

    run tests in IE

--- test/support/treo.js
+++ test/support/treo.js
@@ -1,9 +1,10 @@
-require('./treo-ie')
+var ie = require('./treo-ie')
 var treo = require('../../lib')
 var websql = require('treo-websql')
 
 treo.Promise = Promise // set Promise library
 websql(treo) // patch to support WebSQL env
+ie(treo)
 
 /**
  * Expose treo.
